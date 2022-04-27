0649d6daece0135e200ac93eb42ae77fc69ee04d
tikavalery <43121065+tikavalery@users.noreply.github.com>
2022-04-27 16:13:41 -0700

    uploading fashion social media app

---
+++ keys2.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGOURI: "mongodb+srv://tikavalery:TjLuM6pCy9XHiHAm@cluster0.vujs9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET: "banadzem",
+       SENDGRID_API:"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",
+   EMAIL:process.env.EMAIL
+}
